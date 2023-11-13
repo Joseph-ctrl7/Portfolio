ec10a39f239b5650778fe5957c3918198c7e2275
Joseph-ctrl7 <72221329+Joseph-ctrl7@users.noreply.github.com>
2023-11-13 18:39:18 -0500

    project section is now responsive

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', function(){
     const navList = document.querySelector('.navList');
     const nav = document.querySelector('nav');
     const header = document.querySelector('.header');
+    const card_wrapper = document.querySelector(".card-wrapper");
+    const arrowBtns = document.querySelectorAll(".project-container i");
+
 
     //function for hamburger menu animation
     function toggleButton() {
@@ -35,4 +38,38 @@ document.addEventListener('DOMContentLoaded', function(){
     window.addEventListener('load', () => {
         nav.style.transform = 'translateX(0)';
     });
+
+    //function for project sliders
+    let isDragging = false, startX, startScrollLeft;
+    const dragStart = (e) => {
+        isDragging = true;
+        card_wrapper.classList.add("dragging")
+
+        //records initial cursor and scroll position of the card_wrapper
+        startX = e.pageX;
+        startScrollLeft = card_wrapper.scrollLeft;
+    }
+
+    const dragging = (e) => {
+        if(!isDragging) return;
+
+        //update scroll position of card_wrapper based on cursor movement
+        card_wrapper.scrollLeft = startScrollLeft - (e.pageX-startX);
+    }
+
+    const dragStop = () => {
+        isDragging = false;
+        card_wrapper.classList.remove("dragging");
+    }
+    card_wrapper.addEventListener("mousedown", dragStart);
+    card_wrapper.addEventListener("mousemove", dragging);
+    document.addEventListener("mouseup", dragStop);
+
+    //arrows for the project sliders
+    const firstCardWidth = card_wrapper.querySelector(".card").offsetWidth;
+    arrowBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            card_wrapper.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+        })
+    })
 });
